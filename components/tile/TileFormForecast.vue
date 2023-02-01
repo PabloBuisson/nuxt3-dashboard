3b617ea74d54a585d8678916c33a4ca52dd424f8
Pablo Buisson <pablo.buisson@gmail.com>
2023-02-01 08:59:03 +0100

    feat: add "weather forecast" form

--- components/tile/TileFormForecast.vue
+++ components/tile/TileFormForecast.vue
@@ -3,20 +3,26 @@
     <h1>Weather Forecast Tile</h1>
     <form @submit.prevent="onSubmit">
       <input v-model="tile.title" />
-      <div v-if="tile.category === categories.TODOS">
-        <div v-for="(todo, index) in content">
-          <label :for="todo.id">
-            <input v-model="todo.key" />
-          </label>
-          <input
-            :id="todo.id"
-            type="checkbox"
-            :value="todo.id"
-            v-model="todo.value"
-          />
-          <button @click="deleteTodo(index)" type="button">Delete</button>
-        </div>
-        <button @click="addTodo" type="button">Add todo</button>
+      <label for="autocomplete-city">How is the weather in your city ?</label>
+      <input type="text" id="autocomplete-city" v-model="searchTerm" />
+      <ul v-show="searchCities.length > 0">
+        <li
+          v-for="city in searchCities"
+          :key="city.id"
+          @click="selectCity(city)"
+        >
+          {{ city.name }} ({{ city.country }})
+        </li>
+      </ul>
+      <div v-show="selectedCity">
+        <p>
+          Selected city : {{ selectedCity?.name }} ({{ selectedCity?.country }})
+        </p>
+        <p>
+          Current temperature : {{ selectedCity?.current_weather.temperature }}
+        </p>
+        <p>Current time : {{ selectedCity?.current_weather.time }}</p>
+        <p>Current weather : {{ selectedCity?.current_weather.weathercode }}</p>
       </div>
       <button
         class="px-4 py-2 font-semibold bg-cyan-500 text-white rounded shadow-sm"
@@ -29,6 +35,7 @@
 </template>
 
 <script setup lang="ts">
+import { Ref } from "vue";
 import { Tile, TileCategory } from "~~/models/tile";
 
 interface Props {
@@ -41,14 +48,64 @@ interface Todo {
   id: string;
 }
 
-const props = defineProps<Props>();
-const emit = defineEmits(["submit"]);
+interface GeocodingGETResult {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  feature_code: string;
+  country_code: string;
+  admin1_id: number;
+  admin2_id: number;
+  admin3_id: number;
+  admin4_id: number;
+  timezone: string;
+  population: number;
+  postcodes: string[];
+  country_id: number;
+  country: string;
+  admin1: string;
+  admin2: string;
+  admin3: string;
+  admin4: string;
+}
 
-const categories = TileCategory;
+interface GeocodingGETResponse {
+  results: GeocodingGETResult[];
+}
+
+interface ForecastGETResponse {
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  hourly: {
+    time: string[];
+    temperature_2m: number[];
+  };
+  hourly_units: {
+    temperature_2m: string;
+  };
+  current_weather: {
+    time: string;
+    temperature: number;
+    weathercode: number;
+    windspeed: number;
+    winddirection: number;
+  };
+}
+
+const urlGeocoding = "https://geocoding-api.open-meteo.com/v1/search";
+const urlForecast = "https://api.open-meteo.com/v1/forecast";
+const props = defineProps<Props>();
 const tile: Tile = props.tile ?? {
   id: "",
   title: "",
-  category: TileCategory.TODOS,
+  category: TileCategory.WEATHER,
   content: "",
   dateCreation: new Date(),
 };
@@ -63,24 +120,110 @@ const content = props.tile
       },
     ]);
 
+const searchTerm = ref("");
+const searchCities: Ref<GeocodingGETResult[]> = ref([]);
+const selectedCity: Ref<
+  (GeocodingGETResult & ForecastGETResponse) | undefined
+> = ref();
+
+const emit = defineEmits(["submit"]);
+
+watch(searchTerm, (newSearch) => {
+  console.log(newSearch);
+  if (searchTerm.value.length < 3) {
+    searchCities.value = [];
+  } else {
+    fetchCities();
+  }
+});
+
+async function fetchCities() {
+  const {
+    data: response,
+    pending,
+    error,
+    refresh,
+  } = await useFetch<GeocodingGETResponse>(urlGeocoding, {
+    params: { name: searchTerm.value },
+  });
+
+  searchCities.value = response.value!.results;
+}
+
+async function selectCity(city: GeocodingGETResult) {
+  searchTerm.value = "";
+  console.log(`City selected = ${city.name}`);
+
+  const {
+    data: response,
+    pending,
+    error,
+    refresh,
+  } = await useFetch<ForecastGETResponse>(urlForecast, {
+    params: {
+      latitude: city.latitude,
+      longitude: city.longitude,
+      current_weather: true,
+      timezone: city.timezone,
+    },
+  });
+
+  selectedCity.value = { ...city, ...response.value } as GeocodingGETResult &
+    ForecastGETResponse;
+  console.log(selectedCity.value);
+}
+
 function onSubmit() {
   tile.content = content.value;
   emit("submit", tile);
 }
 
-function addTodo() {
-  const todosLength = content.value.length;
-  const lastTodoId = content.value[todosLength - 1].id;
-  content.value.push({
-    key: `Todo${+lastTodoId + 1}`,
-    value: false,
-    id: `${+lastTodoId + 1}`,
-  });
-}
-
-function deleteTodo(index: number) {
-  const todoToDelete = content.value[index];
-  content.value = content.value.filter((todo) => todo.id !== todoToDelete.id);
+function getWeatherLabel(weatherCode: number) {
+  switch (weatherCode) {
+    case 0:
+      return "Clear sky";
+    case 1:
+    case 2:
+    case 3:
+      return "Mainly clear, partly cloudy, and overcast";
+    case 45:
+    case 48:
+      return "Fog and depositing rime fog";
+    case 51:
+    case 53:
+    case 55:
+      return "Drizzle: Light, moderate, and dense intensity";
+    case 56:
+    case 57:
+      return "Freezing Drizzle: Light and dense intensity";
+    case 61:
+    case 63:
+    case 65:
+      return "Rain: Slight, moderate and heavy intensity";
+    case 66:
+    case 67:
+      return "Freezing Rain: Light and heavy intensity";
+    case 71:
+    case 73:
+    case 75:
+      return "Snow fall: Slight, moderate, and heavy intensity";
+    case 77:
+      return "Snow grains";
+    case 80:
+    case 81:
+    case 82:
+      return "Rain showers: Slight, moderate, and violent";
+    case 85:
+    case 86:
+      return "Snow showers slight and heavy";
+    case 95:
+      return "Thunderstorm: Slight or moderate";
+    case 96:
+    case 99:
+      return "Thunderstorm with slight and heavy hail";
+    default:
+      return "Unknown";
+  }
 }
 </script>
 
