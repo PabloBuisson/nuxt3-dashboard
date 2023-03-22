96a3c0bfb061863b2ba595bed989a6d4967b2bea
Pablo Buisson <pablo.buisson@gmail.com>
2023-03-22 07:54:37 +0100

    refactor(ForecastWeekSummary): improve accessibility

--- components/Forecast/ForecastWeekSummary.vue
+++ components/Forecast/ForecastWeekSummary.vue
@@ -4,7 +4,10 @@
       <li class="flex justify-between" v-for="(day, index) in daily.time">
         <span>{{ day }}</span>
         <span class="flex items-center gap-2">
-          <Icon size="30" :name="useWeatherIcon(daily.weathercode[index])" />
+          <div :title="useWeatherLabel(daily.weathercode[index])">
+            <span class="sr-only">{{ useWeatherLabel(daily.weathercode[index]) }}</span>
+            <Icon size="30" :name="useWeatherIcon(daily.weathercode[index])" />
+          </div>
           <span
             >{{ daily.temperature_2m_max[index] }}/{{
               daily.temperature_2m_min[index]
